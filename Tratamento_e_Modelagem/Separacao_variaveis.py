e99059e4b4a98aa87fd9872f218686ee8d60f176
Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>
2024-07-01 20:18:25 -0300

    Create Separacao_variaveis.py

---
+++ Tratamento_e_Modelagem/Separacao_variaveis.py
@@ -0,0 +1,6 @@
+# Separando as variaveis X (x = caracteristicas)
+
+x = data_set_transformado[["Age", "Sex", "Job", "Housing", "Saving accounts", "Checking account",
+                           "Credit amount", "Duration","Purpose", "Risk"]]
+
+x.head()
