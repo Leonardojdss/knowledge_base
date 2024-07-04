a2a6b199d115e1427f61dc76daaa9194400f6a65
Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>
2024-07-04 08:34:17 -0300

    Update Criando_coluna_condicional_dataset_pandas.py

--- Tratamento_e_Modelagem/Criando_coluna_condicional_dataset_pandas.py
+++ Tratamento_e_Modelagem/Criando_coluna_condicional_dataset_pandas.py
@@ -3,6 +3,6 @@
 # Good = 1 = credito aprovado por ser bom pagador
 # Bad = 0 = credito negagado por ser um mal pagador
 
-data_set_original["Target"] = np.where(data_set_original["Risk"] == "good", 1, 0)
+data_set_original["Target"] = np.where((data_set_original["Risk"] == "good"), 1, 0)
 data_set_transformado = data_set_original
 data_set_transformado
