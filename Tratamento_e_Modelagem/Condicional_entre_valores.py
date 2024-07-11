e161e51db85cf594ea23a09a7272ad61ce9447ee
Leonardo José da Silva <124636951+Leonardojdss@users.noreply.github.com>
2024-07-10 23:45:47 -0300

    Update Condicional_entre_valores.py

--- Tratamento_e_Modelagem/Condicional_entre_valores.py
+++ Tratamento_e_Modelagem/Condicional_entre_valores.py
@@ -1,5 +1,7 @@
 #Aplicar as políticas do banco para obter a variavel target
 
+import numpy as np
+
 # 1 Limite de crédito 
 
 dataset_credit_risk["Nome_coluna ou coluna existente"] = np.where((dataset_credit_risk["coluna"] >= 8000) & (dataset_credit_risk["coluna"] <= 12000), 1, 0)
